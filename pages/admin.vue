9c22b177f807a74079fc1f5e1db102b6cf267b8b
galvinben <galvin.ben@protonmail.com>
2020-04-09 17:36:01 +0100

    netlify open on created

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,13 +4,13 @@
 import netlifyIdentity from 'netlify-identity-widget'
 
 export default {
-  mounted() {
+  mounted() {},
+  created() {
     this.$router.push({ path: '/' })
     netlifyIdentity.open()
     netlifyIdentity.on('login', (user) => {
       this.$store.dispatch('logIn')
     })
   },
-  created() {},
 }
 </script>
