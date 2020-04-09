9e78e139ddb1ff038373bdbed859fa04a9b71ef0
galvinben <galvin.ben@protonmail.com>
2020-04-09 17:33:36 +0100

    forward to / before opening modal

--- pages/admin.vue
+++ pages/admin.vue
@@ -5,13 +5,12 @@ import netlifyIdentity from 'netlify-identity-widget'
 
 export default {
   mounted() {
+    this.$router.push({ path: '/' })
     netlifyIdentity.open()
     netlifyIdentity.on('login', (user) => {
       this.$store.dispatch('logIn')
     })
   },
-  created() {
-    this.$router.push({ path: '/' })
-  },
+  created() {},
 }
 </script>
